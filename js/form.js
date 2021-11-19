1b0a2295ca1a85fdc2c0377f602f74a4e1bd1eb7
Younes <khalfii.youunes@gmail.com>
2021-11-19 22:40:00 +0100

    Update form.js

--- js/form.js
+++ js/form.js
@@ -94,8 +94,8 @@ $(function() {
     const data = getFormData();
     console.log(data);
     $.ajax({ type: "POST", url: "https://51.89.218.94:99/devis-api/v2/src/public/auto", data, dataType: 'json',
-      success: function(data) { window.location.href = '/confirmation.html'; },
-      error: function(error) { console.log("error", error); window.location.href = '/confirmation.html'; }
+      success: function(data) { window.location.href = '/Auto-V1.2/confirmation.html'; },
+      error: function(error) { window.location.href = '/Auto-V1.2/confirmation.html'; }
     })
   })
 })
